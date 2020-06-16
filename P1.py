2312523f721fb1373113ca58b8489095cac7a063
Rishabh <43677827+rishabhshellac@users.noreply.github.com>
2020-06-16 19:16:14 +0530

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 18:20:58 2020
+
+@author: risha
+"""
+def fun1(n):
+    re=0
+    for i in range(n):
+        re=re+i*1
+        return re
+
+print(fun1(10))
